dbefe0c6c2cb7dc67d0b4c9bc9515c1862edd72c
iftesam15 <143783957+iftesam15@users.noreply.github.com>
2024-10-16 12:32:53 +0600

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,43 +1,41 @@
 // src/api.js
 
-import axios from 'axios';
-import { environment } from './environments/environment';
+import axios from "axios";
+import { environment } from "./environments/environment";
 // Create an Axios instance
 const api = axios.create({
-    baseURL: environment.APP_BASE_URL // Base URL for your API
+  baseURL: environment.APP_BASE_URL, // Base URL for your API
 });
 
 // Request interceptor to add Authorization token
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token'); // Assuming the token is stored in localStorage
+  (config) => {
+    const token = localStorage.getItem("token"); // Assuming the token is stored in localStorage
 
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
-        }
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
 
-        return config;
-    },
-    (error) => Promise.reject(error)
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 // Response interceptor to handle errors
 api.interceptors.response.use(
-    (response) => {
-        console.log('response is', response)
-        return response.data
+  (response) => {
+    console.log("response is", response);
+    return response.data;
+  },
 
-    },
-
-
-    (error) => {
-        // Handle errors (e.g., redirect to login if unauthorized)
-        if (error.response && error.response.status === 401) {
-            // Redirect to login page or show an error message
-            window.location.href = '/login'; // Adjust the redirection as needed
-        }
-        return Promise.reject(error);
+  (error) => {
+    // Handle errors (e.g., redirect to login if unauthorized)
+    if (error.response && error.response.status === 401) {
+      // Redirect to login page or show an error message
+      window.location.href = "/login"; // Adjust the redirection as needed
     }
+    return Promise.reject(error);
+  }
 );
 
 export default api;
